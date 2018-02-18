0e405138f9734eec2106a3a04a46c2cb7ce41721
Ricardo Lima <ricardo@ricardolima.ca>
2018-02-18 14:36:47 -0500

    Fix typo

--- install-packages.el
+++ install-packages.el
@@ -1,6 +1,6 @@
 (package-initialize)
 
-(custom-set-variable 'package-selected-packages
+(customize-set-variable 'package-selected-packages
 		     (quote
 		      (csv-mode yaml-mode kivy-mode php-mode flycheck company flx projectile string-inflection yasnippet-snippets which-key ivy-hydra auctex nix-mode elpy yasnippet pandoc-mode markdown-mode magit swiper counsel monokai-theme ivy exec-path-from-shell)))
 
