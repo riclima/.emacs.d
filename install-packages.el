26393e55dfba97e7672f54f319e8746ecd3df6f9
Ricardo Lima <ricardo@ricardolima.ca>
2018-02-18 14:35:01 -0500

    Copy package list to installation script to avoid errors

--- install-packages.el
+++ install-packages.el
@@ -1,6 +1,8 @@
 (package-initialize)
 
-(load "~/.emacs.d/custom.el")
+(custom-set-variable 'package-selected-packages
+		     (quote
+		      (csv-mode yaml-mode kivy-mode php-mode flycheck company flx projectile string-inflection yasnippet-snippets which-key ivy-hydra auctex nix-mode elpy yasnippet pandoc-mode markdown-mode magit swiper counsel monokai-theme ivy exec-path-from-shell)))
 
 (when (not package-archive-contents)
   (package-refresh-contents))
